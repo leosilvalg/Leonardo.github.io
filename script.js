b517d29dff2d2673ab2773c40c6ae66588899a59
Leonardo Gonçalves <leonardorc.geo@gmail.com>
2021-12-23 13:24:32 -0300

    Realizando um teste

---
+++ script.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 500);
+        }
+    })
+})
+
+window.onload = () => {
+    document.getElementById('#gitPixel').click();
+}
